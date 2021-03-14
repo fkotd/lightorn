58ea9196ebc0842b31643d225e58e6638d29216f
Mathilde Grapin <mathilde@fkotd.me>
2021-03-14 18:11:51 +0100

    add window close event

--- WomxnDevelopUbisoftDemo/App.cpp
+++ WomxnDevelopUbisoftDemo/App.cpp
@@ -27,6 +27,16 @@ void App::Run()
 	spawnSystem->Spawn(&world);
 
 	while (window.isOpen()) {
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
 		renderSystem->Render(&world, &window);
 	}
 }
